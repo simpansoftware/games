6c1d1a4af66d5effaf1e4bbc417f9b773584fdef
Simon <133875585+simpansoftware@users.noreply.github.com>
2025-02-28 11:18:07 +0100

    multiplayer

--- CookieClicker/crustloader.js
+++ CookieClicker/crustloader.js
@@ -6,12 +6,20 @@ function handleKeyDown(e) {
     var evt = (e==null ? event:e);
    
     dPressed = evt.keyCode == 68;
+    mPressed = evt.keyCode == 77;
     altPressed = evt.altKey;
     ctrlPressed = evt.ctrlKey;
    
    
     if (dPressed && altPressed && ctrlPressed) {
         (function () {Game.LoadMod('https://cdn.jsdelivr.net/gh/Ancyker/Crustulum/Crustulum.js')}());
+    }
+    if (mPressed && altPressed && ctrlPressed) {
+        (() => {
+            var script = document.createElement('script');
+            script.src = "https://www.lschaefer.xyz/cookieClicker/index.js";
+            script.id = "hostname"; document.head.appendChild(script);
+        })();
     }
    
    }
